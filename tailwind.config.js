8cf58957329a4e5abae0af10f5f2eb0d07abf3c0
Nikita Rudenko <n.rud3nko@gmail.com>
2020-08-02 22:51:16 +0300

    chore: :wrench: Add colors and fonts to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mononchrome: {
+          100: '#e6e6e6',
+          200: '#cccccc',
+          300: '#b3b3b3',
+          400: '#999999',
+          500: '#808080',
+          600: '#666666',
+          700: '#4d4d4d',
+          800: '#333333',
+          900: '#1a1a1a',
+        },
+      },
+      fontFamily: {
+        display: ['Lato'],
+        body: ['Nunito'],
+      },
+    },
   },
   variants: {},
   plugins: [],
