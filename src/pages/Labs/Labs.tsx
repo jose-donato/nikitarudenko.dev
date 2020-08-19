7eeabd70728f6223c5df48a1d4d3664d38a7d494
Nikita Rudenko <n.rud3nko@gmail.com>
2020-08-19 23:00:15 +0300

    feat: :sparkles: Add Labs page

---
+++ src/pages/Labs/Labs.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link'
+
+import Card from '@components/Card'
+import Layout from '@components/Layout'
+import Tag from '@components/Tag'
+import { TProject } from '@typings/contentTypes'
+
+type Props = {
+  projects: TProject[]
+}
+
+const Labs = ({ projects }: Props): JSX.Element => (
+  <Layout>
+    <Layout.Header />
+    <Layout.Body className="px-4 gradient-light">
+      <div className="flex items-center justify-center px-2 py-24 lg:px-6 text-mononchrome-800">
+        <h1 className="px-4 text-5xl font-bold duration-200 transform bg-yellow-400 shadow-md select-none font-display">
+          Labs
+        </h1>
+      </div>
+
+      <section className="min-h-screen pb-8 md:pb-16">
+        <div className="max-w-screen-md mx-auto">
+          <ul className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            {projects.map(({ href, title, id, excerpt, technologies }) => {
+              return (
+                <Card
+                  className="block transition duration-200 transform rounded-md shadow-md cursor-pointer font-body hover:bg-gray-200"
+                  key={id}
+                >
+                  <li>
+                    <Link href={href}>
+                      <a className="">
+                        <div className="flex flex-col p-5">
+                          <div className="text-base font-bold md:text-lg text-mononchrome-900">
+                            {title}
+                          </div>
+                          <div className="mt-1 text-sm font-bold md:mt-2 md:text-base text-mononchrome-400">
+                            {excerpt}
+                          </div>
+                          <div className="mt-4 ml-auto">
+                            {technologies.map((t) => (
+                              <Tag key={t} label={t} />
+                            ))}
+                          </div>
+                        </div>
+                      </a>
+                    </Link>
+                  </li>
+                </Card>
+              )
+            })}
+          </ul>
+        </div>
+      </section>
+    </Layout.Body>
+    <Layout.Footer />
+  </Layout>
+)
+
+export default Labs
