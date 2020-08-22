36a2b2926b5e6607a47120281437ebbecb63a931
Nikita Rudenko <n.rud3nko@gmail.com>
2020-08-23 02:16:18 +0300

    refactor: :alembic: Try self-hosted fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 
 import '@styles/tailwind.css'
+import '@styles/fonts.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
