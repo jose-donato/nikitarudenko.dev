7188ea13af4636f5b0963d8c7cf3aee7eab98204
Nikita Rudenko <n.rud3nko@gmail.com>
2020-08-24 22:59:32 +0300

    feat: :sparkles: Add social links

---
+++ src/constants/socialLinks.tsx
@@ -0,0 +1,31 @@
+import {
+  EmailIcon,
+  GithubIcon,
+  LinkedInIcon,
+  TwitterIcon,
+} from '@components/SVG'
+
+const SOCIAL_LINKS = [
+  {
+    label: 'Twitter',
+    emoji: <TwitterIcon />,
+    href: '/',
+  },
+  {
+    label: 'Github',
+    emoji: <GithubIcon />,
+    href: '/',
+  },
+  {
+    label: 'LinkedIn',
+    emoji: <LinkedInIcon />,
+    href: '/',
+  },
+  {
+    label: 'Email',
+    emoji: <EmailIcon />,
+    href: '/',
+  },
+]
+
+export default SOCIAL_LINKS
