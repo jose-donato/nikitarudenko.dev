af73a63cee50c4e76dedfdebbcfa8cfbcaaa121d
Nikita Rudenko <n.rud3nko@gmail.com>
2020-09-01 23:00:26 +0300

    feat: :mag: Improve SEO

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import NextDocument, { Head, Main, NextScript } from 'next/document'
+
+class Document extends NextDocument<{ css: string }> {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
+
+export default Document
