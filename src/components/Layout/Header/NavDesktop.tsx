ac7119df55955304ca92be08f2ba8a6cb063f31c
Nikita Rudenko <n.rud3nko@gmail.com>
2020-09-02 21:16:34 +0300

    refactor: :fire: Get rid of useWidth hook and use @media

--- src/components/Layout/Header/NavDesktop.tsx
+++ src/components/Layout/Header/NavDesktop.tsx
@@ -4,7 +4,7 @@ import HeaderLink from './HeaderLink'
 
 const NavDesktop = () => {
   return (
-    <nav className="flex justify-center max-w-screen-md grid-flow-col gap-6 py-4 mx-auto text-sm font-bold font-body md:text-base text-monochrome-800">
+    <nav className="justify-center hidden max-w-screen-md grid-flow-col gap-6 py-4 mx-auto text-sm font-bold md:flex font-body md:text-base text-monochrome-800">
       {INTERNAL_LINKS_LIST.map(({ href, label, emoji }) => {
         return (
           <HeaderLink key={label} label={label} href={href} emoji={emoji} />
