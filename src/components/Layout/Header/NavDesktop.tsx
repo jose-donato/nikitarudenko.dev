f807ff9ccd6a1121060f900eac5102197e451960
Nikita Rudenko <rdnkta@gmail.com>
2020-09-27 19:49:59 +0300

    fix: :bug: Fix broken nav in Safari

--- src/components/Layout/Header/NavDesktop.tsx
+++ src/components/Layout/Header/NavDesktop.tsx
@@ -5,7 +5,7 @@ import HeaderLink from './HeaderLink'
 
 const NavDesktop = () => {
   return (
-    <nav className="relative justify-center hidden max-w-screen-md grid-flow-col gap-6 py-4 mx-auto text-sm font-bold md:flex font-body md:text-base">
+    <nav className="relative items-center justify-center hidden max-w-screen-md grid-flow-col gap-6 py-4 mx-auto text-sm font-bold md:grid font-body md:text-base">
       {INTERNAL_LINKS_LIST.map(({ href, label, emoji }) => {
         return (
           <HeaderLink key={label} label={label} href={href} emoji={emoji} />
